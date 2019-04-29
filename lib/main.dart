727830a7da685410a70f46c6e950c31dfe7f743d
KimKosit <user_kho_kim@hotmail.com>
2019-04-29 11:19:05 +0700

    Theme

--- lib/main.dart
+++ lib/main.dart
@@ -27,6 +27,7 @@ class MyAppState extends State<MyApp>{
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.amber,),
       title: "Flutter Deno",
       home: BlocProvider<CounterBloc>(
         bloc: _counterBloc,
